07264d90858cccb560811387684b0886c02ab725
alptarla <alptarla@gmail.com>
2022-03-28 12:09:20 +0300

    Add pie chart with victory-native

--- screens/AnalyticsScreen.tsx
+++ screens/AnalyticsScreen.tsx
@@ -1,14 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import useMood from '../hooks/useMood'
+import { groupBy } from 'lodash'
+import { VictoryPie } from 'victory-native'
+import { theme } from '../theme'
 
 const AnalyticsScreen = () => {
+  const { moodList } = useMood()
+
+  const data = Object.entries(groupBy(moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    })
+  )
+
   return (
-    <View>
-      <Text>AnalyticsScreen</Text>
+    <View style={styles.screen}>
+      <VictoryPie
+        data={data}
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        style={{ labels: { fontSize: 30 } }}
+      />
     </View>
   )
 }
 
 export default AnalyticsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
